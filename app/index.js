204ff78088f546d91f1088252fab62d9b4b6341b
heltonandreazza <helton.prg@gmail.com>
2016-11-02 16:25:03 -0200

    remove redis

--- app/index.js
+++ app/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const config = require('./config');
-const redis = require('redis').createClient;
-const adapter = require('socket.io-redis');
+// * setting up redis
+// const config = require('./config');
+// const redis = require('redis').createClient;
+// const adapter = require('socket.io-redis');
+// * setting up redis
 
 //social authentication logic
 require('./auth')();
@@ -12,18 +14,20 @@ let ioServer = app => {
     app.locals.chatrooms = [];
     const server = require('http').Server(app);
     const io = require('socket.io')(server);
-    io.set('transports', ['websocket']);
-    let pubClient = redis(config.redis.port, config.redis.host, {
-        auth_pass: config.redis.password
-    });
-    let subClient = redis(config.redis.port, config.redis.host, {
-        return_buffers: true,
-        auth_pass: config.redis.password
-    });
-    io.adapter(adapter({
-        pubClient,
-        subClient
-    }))
+    // * setting up redis
+    // io.set('transports', ['websocket']);
+    // let pubClient = redis(config.redis.port, config.redis.host, {
+    //     auth_pass: config.redis.password
+    // });
+    // let subClient = redis(config.redis.port, config.redis.host, {
+    //     return_buffers: true,
+    //     auth_pass: config.redis.password
+    // });
+    // io.adapter(adapter({
+    //     pubClient,
+    //     subClient
+    // }))
+    // * setting up redis
     io.use((socket, next) => {
         require('./sessions')(socket.request, {}, next);
     })
